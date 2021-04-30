b95a0bf6c5e01ee5a2b02dfae804021275f92f0d
Sylack <ilarumbgim1@educacion.navarra.es>
2021-04-30 15:11:36 +0000

    Rubify

---
+++ main.rb
@@ -0,0 +1,13 @@
+Voltage = 20
+Intensity = 2 # Ampers
+Resistances = [
+    12,
+    4,
+    6
+]
+
+for resistor in Resistances do
+    puts "Volatge = #{resistor*Intensity}V"
+
+    puts "i = #{i}"
+end
